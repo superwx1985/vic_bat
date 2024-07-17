41e2b39c3ddbde7f5a08049f26a90cbbafb93ac8
superwx1985 <superwx1985@gmail.com>
2024-07-17 10:53:47 +0800

    Update 修改DNS.bat

--- 修改DNS.bat
+++ 修改DNS.bat
@@ -1,13 +1,43 @@
 @echo off
+:: Check for Administrator rights
+openfiles >nul 2>&1
+if '%errorlevel%' neq '0' (
+    echo Requesting administrator rights...
+    goto UACPrompt
+) else ( 
+    goto gotAdmin
+)
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~f0""", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /b
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+@echo off
 setlocal enabledelayedexpansion
 
 REM 设置域名和DNS服务器
 set /p DOMAIN=请输入需要解析的域名: 
-set DNS_SERVER=114.114.114.114
+set DEFAULT_DNS=114.114.114.114
+set /p DNS=请输入DNS(默认是%DEFAULT_DNS%): 
+if "%DNS%"=="" (
+    set DNS=%DEFAULT_DNS%
+)
 
 REM 查询域名的DNS
-for /f "tokens=2" %%a in ('nslookup %DOMAIN% %DNS_SERVER% ^| find "Addresses:"') do set IP=%%a
-echo ##### 通过 %DNS_SERVER% 解析到 %DOMAIN% 的IP是 %IP% #####
+for /f "tokens=2" %%a in ('nslookup -type^=A %DOMAIN% %DNS% ^| find "Addresses:"') do set IP=%%a
+if "%IP%"=="" (
+    echo ##### 通过 %DNS% 没有解析到 %DOMAIN% 的IP #####
+    goto end
+) else (
+    echo ##### 通过 %DNS% 解析到 %DOMAIN% 的IP是 %IP% #####
+)
 
 REM 检查域名是否已经存在于hosts文件
 findstr /i %DOMAIN% %windir%\System32\drivers\etc\hosts >nul
@@ -21,7 +51,7 @@ if %errorlevel% equ 0 (
     )
 ) else (
     REM 如果不存在，则添加新条目
-    echo %IP%    %DOMAIN%>> %windir%\System32\drivers\etc\hosts
+    echo %IP% %DOMAIN%>> %windir%\System32\drivers\etc\hosts
     goto flushdns
 )
 
@@ -49,4 +79,6 @@ echo ##### 更新hosts文件 #####
 REM 刷新DNS缓存
 ipconfig /flushdns
 
+:end
+pause
 goto :eof
